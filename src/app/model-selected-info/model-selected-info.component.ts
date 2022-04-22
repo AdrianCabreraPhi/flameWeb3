c2591cdd8475089de29ead2b45ec79218f350a63
AdrianCabreraPhi <adridominican@gmail.com>
2022-04-22 18:52:03 +0200

    models: changed info panel style

--- src/app/model-selected-info/model-selected-info.component.ts
+++ src/app/model-selected-info/model-selected-info.component.ts
@@ -3,9 +3,11 @@ import { Model} from '../Globals';
 
 @Component({
   selector: 'app-model-selected-info',
-  template: `<div class="selected-list">
+  template: `
+  <hr>
+  <div class="selected-list">
   <div *ngFor="let model of this.model.listModelsSelected">
-      <span>* {{model.name}} v.{{model.version}} {{model.quantitative}} |</span> {{model.type}} |<span>{{model.endpoint}} </span>
+      <span> {{model.name}} v.{{model.version}} {{model.quantitative}} |</span> {{model.type}} |<span>{{model.endpoint}} </span>
   </div> 
 </div>`,
   // templateUrl: './model-selected-info.component.html',
