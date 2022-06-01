5042a2dcc2c3350acc3e44d52fb0837e25b948fb
AdrianCabreraPhi <adridominican@gmail.com>
2022-06-01 15:36:35 +0200

    including sorting in dtTable

--- src/app/multiple-prediction/multiple-prediction.component.ts
+++ src/app/multiple-prediction/multiple-prediction.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { CommonFunctions } from '../common.functions';
 import { CommonService } from '../common.service';
 import { Prediction, Globals, Model } from '../Globals';
 import { PredictorService } from '../manage-models/predictor.service';
 import 'datatables.net-bs4';
 import chroma from "chroma-js";
+import { ViewChild } from '@angular/core';
 declare var $: any;
 @Component({
   selector: 'app-multiple-prediction',
@@ -23,7 +24,8 @@ export class MultiplePredictionComponent implements OnInit {
     public commonService: CommonService,
     public commonFunctions: CommonFunctions,
     public globals: Globals,
-    private model: Model
+    private model: Model,
+    private renderer2: Renderer2,
   ) { }
 
 
@@ -40,37 +42,36 @@ export class MultiplePredictionComponent implements OnInit {
     const text = compound + "\n" + this.prediction.profileSummary['endpoint'][column] + "\n" + val;
     event.target.setAttribute('title', text);
   }
-
-
-  showPrediction(event, molIndex) {
+  showPrediction(event, molIndex,td) {
     const column = event.target._DT_CellIndex.column - 1;
-    this.selectedClass(event);
     const modelName = this.prediction.profileSummary['endpoint'][column] + '-' + this.prediction.profileSummary['version'][column];
     const modelObj = this.model.listModels[modelName];
     this.prediction.modelName = this.prediction.profileSummary['endpoint'][column];
     this.prediction.modelVersion = this.prediction.profileSummary['version'][column];
     this.prediction.modelID = modelObj['modelID'];    
     this.commonService.setMolAndModelIndex(molIndex,column);
-      
+    this.selectedClass(event,td);      
       }
 
   /**
    * Function to add specific styles to the selected prediction.
    * @param event 
    */
-  selectedClass(event) {
-    if((this.Smodel,this.Smol)!=undefined){
-      $('#dataTablePrediction thead th:eq('+this.Smodel+')').css("background",'white');
-      $("#dataTablePrediction tr:eq("+this.Smol+") td:first").css("background", "white");
-    }
+  selectedClass(event,td) {
+  if((this.Smol,this.Smodel)!=undefined){
+    this.renderer2.setStyle(this.Smol,'background','white')
+    $('#dataTablePrediction thead th:eq('+this.Smodel+')').css("background",'white');
+  }
     this.Smodel = event.target._DT_CellIndex.column;
-    this.Smol = event.target._DT_CellIndex.row +1;
+    this.Smol = td;
+    this.renderer2.setStyle(td,'background','#f7f9ea')
     $('#dataTablePrediction thead th:eq('+this.Smodel+')').css("background",'#f7f9ea');
-    $("#dataTablePrediction tr:eq("+this.Smol+") td:first").css("background", "#f7f9ea");
     
+
     if (this.prevSelection) this.prevSelection.classList.remove('selected');
     this.prevSelection = event.target;
     event.target.classList.add('selected');
+    
   }
   getProfileSummary() {
     this.prediction.profileSummary = undefined;
@@ -86,11 +87,11 @@ export class MultiplePredictionComponent implements OnInit {
               autoWidth: false,
               destroy: true,
               paging: false,
-              ordering: false,
+              ordering: true,
               searching: false,
               info: false,
               })
-          }, 20)
+          }, 30)
         }
       },
       (error) => {
