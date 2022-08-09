dd4f906481fcf455ea55b582f7171aae466ada35
AdrianCabreraPhi <adridominican@gmail.com>
2022-08-09 16:20:03 +0200

    profile execution

--- src/app/predict-button/predict-button.component.ts
+++ src/app/predict-button/predict-button.component.ts
@@ -26,7 +26,7 @@ export class PredictButtonComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.predictionName = 'Profile1'
+    this.predictionName = 'Profile3'
     $(function () {
       $('[data-toggle="popover"]').popover();
     });
@@ -34,6 +34,7 @@ export class PredictButtonComponent implements OnInit {
       (value) => (this.isValidCompound = value)
     );
   }
+
   select_prediction() {
     if (this.compound.input_file) {
       this.predict();
@@ -45,38 +46,43 @@ export class PredictButtonComponent implements OnInit {
       this.predictInputList();
     }
   }
+
   profileNameChange(){
     this.isvalidProfile = true;
     const letters = /^[A-Za-z0-9_]+$/;
-    if (!this.prediction.profileName.match(letters) || this.prediction.profileName == '') {
+    if (!this.predictionName.match(letters) || this.predictionName == '') {
       this.isvalidProfile = false;
     }
   }
+
   predictStructure() {
     this.filterModels();
+    const inserted = this.toastr.info('Running!', 'Prediction ' + this.predictionName, {
+      disableTimeOut: true,
+      positionClass: 'toast-top-right',
+    });
     this.service
       .predictSketchStructure(
-        this.prediction.profileName,
+        this.predictionName,
         this.compound.sketchstructure['result'],
         this.compound.sketchstructure['name'],
         JSON.stringify(this.endpoints),
         JSON.stringify(this.versions)
       )
       .subscribe(
-        (result) => {
-          // I need boolean. not string message
-          if (result) {
-            this.commonService.setPredictionExec(true);
-            this.toastr.success(
-              this.compound.sketchstructure['name'],
-              'PREDICTION COMPLETED',
-              {
-                timeOut: 4000,
-                positionClass: 'toast-top-right',
-                progressBar: true,
-              }
-            );
-          }
+        result => {
+          let iter = 0;
+          const intervalId = setInterval(()=> {
+            if(iter < 500){
+              this.checkProfile(this.predictionName,inserted,intervalId)
+            }else{
+              clearInterval(intervalId)
+              this.toastr.clear(inserted.toastId);
+              this.toastr.warning( 'Prediction ' + this.predictionName + ' \n Time Out' , 'Warning', {
+                timeOut: 10000, positionClass: 'toast-top-right'});
+            }
+            iter +=1;
+          },2000)
         },
         (error) => {
           console.log(error);
@@ -85,47 +91,75 @@ export class PredictButtonComponent implements OnInit {
   }
   predict() {
     this.filterModels();
-
-    this.toastr.info('Running!', 'Prediction ' + this.prediction.profileName, {
+    const inserted = this.toastr.info('Running!', 'Prediction ' + this.predictionName, {
       disableTimeOut: true,
       positionClass: 'toast-top-right',
     });
 
     this.service
       .predictInputFile(
-        this.prediction.profileName,
+        this.predictionName,
         this.compound.input_file['result'],
         JSON.stringify(this.endpoints),
         JSON.stringify(this.versions)
       )
       .subscribe(
-        (result) => {
-          if (result) {
-            console.log(result);
-            this.toastr.clear();
-            this.commonService.setPredictionExec(true);
-            this.toastr.success(
-              this.compound.input_file['name'],
-              'PREDICTION COMPLETED',
-              {
-                timeOut: 4000,
-                positionClass: 'toast-top-right',
-                progressBar: true,
-              }
-            );
-          }
+        result => {
+          let iter = 0;
+          const intervalId = setInterval(()=> {
+           if(iter < 500){
+
+          this.checkProfile(this.predictionName,inserted,intervalId)
+           }else {
+            this.toastr.clear(inserted.toastId);
+            this.toastr.warning( 'Prediction ' + this.predictionName + ' \n Time Out' , 'Warning', {
+              timeOut: 10000, positionClass: 'toast-top-right'});
+
+           }
+           iter+=1
+
+          },2000)
         },
-        (error) => {
+        error => {
           console.log('error');
         }
       );
   }
-  
+  checkProfile(name,inserted,intervalId){
+     this.service.profileSummary(name).subscribe(
+       result => {
+        if (result['aborted']) {
+          this.toastr.clear(inserted.toastId);
+          this.toastr.error("Prediction \"" + name + "\" task has not completed. Check the browser console for more information", 
+            'Aborted', {timeOut: 10000, positionClass: 'toast-top-right'});
+          console.log('ERROR report produced by prediction task ', name);
+          console.log(result['aborted']);
+          clearInterval(intervalId);
+          return;
+        }
+        if (!result ['waiting']) {
+          this.toastr.clear(inserted.toastId);
+          if (result['error']){
+            this.toastr.warning('Profile ' + name + ' finished with error ' + result['error'] , 'PREDICTION COMPLETED', {
+              timeOut: 5000, positionClass: 'toast-top-right'});
+          }
+          else {
+             this.toastr.success('Profile ' + name + ' created' , 'PREDICTION COMPLETED', {
+              timeOut: 5000, positionClass: 'toast-top-right'});
+              this.commonService.setPredictionExec(true);
+          }
+          clearInterval(intervalId);
+          $('#dataTablePredictions').DataTable().destroy();
+        }
+       }
+     )
+
+  }
   // TO DO
   predictInputList() {
     this.filterModels();
     this.service.predictInputList(
-      this.prediction.profileName,
+      this.predictionName,
       this.compound.input_list['result'],
       this.compound.input_list['name'],
       JSON.stringify(this.endpoints),
