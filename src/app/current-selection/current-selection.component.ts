5153b9980b9436836405cdcc615fd8e024618322
AdrianCabreraPhi <adridominican@gmail.com>
2022-08-11 20:14:34 +0200

    selected models

--- src/app/current-selection/current-selection.component.ts
+++ src/app/current-selection/current-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Model } from '../Globals';
 
 @Component({
   selector: 'app-current-selection',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentSelectionComponent implements OnInit {
 
-  constructor() { }
+  constructor( public model: Model) { }
 
   ngOnInit(): void {
   }
