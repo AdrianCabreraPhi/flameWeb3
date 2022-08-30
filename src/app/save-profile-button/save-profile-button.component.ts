8b3816b269e9fb4d55a2176694eac847e7439c97
AdrianCabreraPhi <adridominican@gmail.com>
2022-08-30 11:12:54 +0200

    modal implemented

--- src/app/save-profile-button/save-profile-button.component.ts
+++ src/app/save-profile-button/save-profile-button.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Model } from '../Globals';
 import { PredictorService } from '../manage-models/predictor.service';
-
+declare var $: any;
 @Component({
   selector: 'app-save-profile-button',
   templateUrl: './save-profile-button.component.html',
@@ -12,18 +12,29 @@ export class SaveProfileButtonComponent implements OnInit {
   constructor(public model: Model,private service: PredictorService) { }
   endpoints = [];
   versions = [];
+  name: string = 'collection';
+  isvalidName: boolean = true;
 
   ngOnInit(): void {
   }
 
   saveCollection(){
   this.filterModels();
-
-    this.service.collection("frontend",JSON.stringify(this.endpoints),JSON.stringify(this.versions)).subscribe(result =>{
+    this.service.collection(this.name,JSON.stringify(this.endpoints),JSON.stringify(this.versions)).subscribe(result =>{
       console.log(result)
     } )
-    
+    $('#saveModal').modal('hide')
   }
+
+  collectionNameChange(){
+    const letters = /^[A-Za-z0-9_]+$/;
+    if (!this.name.match(letters) || this.name == '') {
+      this.isvalidName = false;
+    }else{
+      this.isvalidName  = true;
+    }
+  }
+
   filterModels() {
     this.endpoints = [];
     this.versions = [];
