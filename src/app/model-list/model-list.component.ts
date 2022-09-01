e074068230c3a31c38c770b1e6a0d0726443cab0
AdrianCabreraPhi <adriancabproven@gmail.com>
2022-09-01 12:41:58 +0200

    bug fixed: load collection

--- src/app/model-list/model-list.component.ts
+++ src/app/model-list/model-list.component.ts
@@ -23,7 +23,7 @@ export class ModelListComponent implements OnInit {
     private commonService: CommonService,
     public compound: Compound,
     private toastr: ToastrService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.commonService.loadCollection$.subscribe(res => {
@@ -55,43 +55,52 @@ export class ModelListComponent implements OnInit {
   //       );
   //   }
   // }
-  checkCollection(collect: Object){
-       var notFound = []
-       for (let i = 0; i < collect['endpoints'].length; i++) {
-         var found = false
-         const element = collect['endpoints'][i]+'-'+collect['versions'][i]
-         for (let key in this.model.listModels){
-             if(element == key){
-               found = true
-             }
-         }
-         if(!found) notFound.push(element)
-       }    
-       if(notFound.length > 0){
-         console.log(notFound)
-         this.toastr.error( 'Collection ' + collect['name'] + ' \n Contains models not found in the current repository'+ '\n'+notFound , 'Failed', {
-          timeOut: 10000, positionClass: 'toast-top-right'});
-       }else{
-         this.loadCollection(collect);
-       }
+  checkCollection(collect: Object) {
+    var notFound = []
+    for (let i = 0; i < collect['endpoints'].length; i++) {
+      var found = false
+      const element = collect['endpoints'][i] + '-' + collect['versions'][i]
+      for (let key in this.model.listModels) {
+        if (element == key) {
+          found = true
+        }
+      }
+      if (!found) notFound.push(element)
+    }
+    if (notFound.length > 0) {
+      console.log(notFound)
+      this.toastr.error('Collection ' + collect['name'] + ' \n Contains models not found in the current repository' + '\n' + notFound, 'Failed', {
+        timeOut: 10000, positionClass: 'toast-top-right'
+      });
+    } else {
+      this.loadCollection(collect);
+    }
   }
-  loadCollection(collect: Object){
+  loadCollection(collect: Object) {
+    this.model.listModelsSelected = []
+
+    $('#dataTableModels').DataTable().rows().every(function (idx, tableLoop, rowLoop) {
+      var node = this.node()
+      let checkbox = node.childNodes[0].childNodes[0]
+      checkbox.checked = false; //first clean all checkboxes
+    });
+
     const self = this;
-    $('#dataTableModels').DataTable().rows().every( function ( idx, tableLoop, rowLoop ) {
+    $('#dataTableModels').DataTable().rows().every(function (idx, tableLoop, rowLoop) {
       var data = this.data();
       var node = this.node()
-      let checkbox =  node.childNodes[0].childNodes[0]
+      let checkbox = node.childNodes[0].childNodes[0]
       for (let i = 0; i < collect['endpoints'].length; i++) {
-          if(data[2] == collect['endpoints'][i] && data[3] == collect['versions'][i] ){
-              checkbox.checked = true;
-              const obj = {
-                name: data[2],
-                version: data[3],
-              };
-              self.model.listModelsSelected.push(obj)
-          }
-      }  
-  } );
+        if (data[2] == collect['endpoints'][i] && data[3] == collect['versions'][i]) {
+          checkbox.checked = true;
+          const obj = {
+            name: data[2],
+            version: data[3],
+          };
+          self.model.listModelsSelected.push(obj)
+        }
+      }
+    });
   }
   onChange(name, version, event): void {
     const obj = {
